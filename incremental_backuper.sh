d979a4807b4a5cb3a1515b602cfaf2b6d95d0e5d
GaryZhu <Gary@movio.co>
2016-10-18 21:52:49 +1300

    gets keyspaces from SYSTEM_SCHEMA

--- incremental_backuper.sh
+++ incremental_backuper.sh
@@ -8,25 +8,31 @@ main () {
   local BACKUP_ROOT_FOLER="/var/lib/cassandra/custom_backups"
   local BACKUP_FOLDER=${BACKUP_ROOT_FOLER}/$DATE
   mkdir -p $BACKUP_FOLDER
-  KEYSPACES=$(ls $BACKUP_SOURCE_FOLDER | grep -v "system")
-  for keyspace in $KEYSPACES
-  do
-    # Flush all data in memtable to sstable
-    echo "$(date '+%d/%m/%Y %H:%M:%S') Start flushing incremental changes from memtable to sstable for keysapce: $keyspace" >> log_incremental_backups.txt
-    nodetool -h localhost -p 7199 flush $keyspace
+  
+  KEYSPACE_QUERY=$(cqlsh -e "SELECT KEYSPACE_NAME FROM SYSTEM_SCHEMA.KEYSPACES" -u cassandra -p cassandra)
+  read -ra KEYSPACES <<< $KEYSPACE_QUERY
 
-    # Copy the incremental backup keyspace by keyspace
-    for dir in $BACKUP_SOURCE_FOLDER/$keyspace/*
-    do
-      TARGET_FOLDER=$BACKUP_FOLDER/$keyspace/${dir##*/}/
-      SOURCE_FOLDER=$dir/backups
-      if [ -d "$SOURCE_FOLDER" ]; then
-        echo "$(date '+%d/%m/%Y %H:%M:%S') Start copying incremental changes from: $TARGET_FOLDER to: $TARGET_FOLDER" >> log_incremental_backups.txt
-        mkdir -p $TARGET_FOLDER
-        cp -r -p $SOURCE_FOLDER $TARGET_FOLDER
-        #rm -r $SOURCE_FOLDER
-      fi
-    done
+  for KEYSPACE in "${KEYSPACES[@]}"
+  do
+    if [[ $KEYSPACE != *"keyspace_name"* ]] && [[ $KEYSPACE != *"--"* ]] && [[ $KEYSPACE != *"("* ]] && [[ $KEYSPACE != *")"* ]]
+    then
+      # Flush all data in memtable to sstable
+      echo "$(date '+%d/%m/%Y %H:%M:%S') Start flushing incremental changes from memtable to sstable for keysapce: $keyspace" >> log_incremental_backups.txt
+      nodetool -h localhost -p 7199 flush $keyspace
+
+      # Copy the incremental backup keyspace by keyspace
+      for dir in $BACKUP_SOURCE_FOLDER/$keyspace/*
+      do
+        TARGET_FOLDER=$BACKUP_FOLDER/$keyspace/${dir##*/}/
+        SOURCE_FOLDER=$dir/backups
+        if [ -d "$SOURCE_FOLDER" ]; then
+          echo "$(date '+%d/%m/%Y %H:%M:%S') Start copying incremental changes from: $TARGET_FOLDER to: $TARGET_FOLDER" >> log_incremental_backups.txt
+          mkdir -p $TARGET_FOLDER
+          cp -r -p $SOURCE_FOLDER $TARGET_FOLDER
+          #rm -r $SOURCE_FOLDER
+        fi
+      done
+    fi
   done
 
   tar -czf $BACKUP_FOLDER.tgz $BACKUP_FOLDER >> /dev/null 2>&1
